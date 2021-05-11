721f14181f042a5f75edf53d740509efdbf20775
Gabriel Walsh <brotherclone@gmail.com>
2021-05-11 10:38:41 -0400

    some basics

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,19 @@ class HomeController < ApplicationController
 
   def index
     @show_nav = true
-    case_studies = CaseStudy.last(2)
+    # ToDo: Add real data
+
     @case_study_cards = []
-    case_studies.each do |case_study|
-      # ToDo: Add real data
-      @case_study_cards << CaseStudyCardItem.new( case_study.id, 'img.png', 'My Thing', 'description')
+
+    # case_studies = CaseStudy.last(2)
+    # case_studies.each do |case_study|
+    #   @case_study_cards << CaseStudyCardItem.new( case_study.id, 'img.png', 'My Thing', 'description')
+    # end
+
+    (0...3).each do |case_study|
+       @case_study_cards << CaseStudyCardItem.new( 1, 'https://via.placeholder.com/200', 'My Thing', 'description')
     end
+
   end
 
 end
