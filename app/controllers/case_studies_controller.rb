9d6ac42e0fea4fc77eeebdb8f629c8dcdd1d718a
Gabriel Walsh <brotherclone@gmail.com>
2021-05-18 09:28:14 -0400

    this closes #41

--- app/controllers/case_studies_controller.rb
+++ app/controllers/case_studies_controller.rb
@@ -5,6 +5,18 @@ class CaseStudiesController < ApplicationController
   def index
     @show_nav = true
     @case_studies = CaseStudy.all
+    # ToDo: Add real data
+
+    @case_study_cards = []
+
+    # case_studies = CaseStudy.last(2)
+    # case_studies.each do |case_study|
+    #   @case_study_cards << CaseStudyCardItem.new( case_study.id, 'img.png', 'My Thing', 'description')
+    # end
+
+    (0...3).each do |case_study|
+      @case_study_cards << CaseStudyCardItem.new( 1, 'https://via.placeholder.com/200', 'My Thing', 'description')
+    end
   end
 
   def new
