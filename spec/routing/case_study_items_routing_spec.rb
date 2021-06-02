509d18b6ff32f301d94e28c3189dede307d9ce46
Gabriel Walsh <brotherclone@gmail.com>
2021-06-02 06:01:14 -0400

    fixed tests

--- spec/routing/case_study_items_routing_spec.rb
+++ spec/routing/case_study_items_routing_spec.rb
@@ -1,38 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe CaseStudiesController, type: :routing do
+RSpec.describe CaseStudyItemsController, type: :routing do
   describe 'routing' do
 
     it 'routes to #index' do
-      expect(:get => '/case_studies').to route_to('case_studies#index')
+      expect(:get => '/case_study_items').to route_to('case_study_items#index')
     end
 
     it 'routes to #new' do
-      expect(:get => '/case_studies/new').to route_to('case_studies#new')
+      expect(:get => '/case_study_items/new').to route_to('case_study_items#new')
     end
 
     it 'routes to #show' do
-      expect(:get => '/case_studies/1').to route_to('case_studies#show', :id => '1')
+      expect(:get => '/case_study_items/1').to route_to('case_study_items#show', :id => '1')
     end
 
     it 'routes to #edit' do
-      expect(:get => '/case_studies/1/edit').to route_to('case_studies#edit', :id => '1')
+      expect(:get => '/case_study_items/1/edit').to route_to('case_study_items#edit', :id => '1')
     end
 
     it 'routes to #create' do
-      expect(:post => '/case_studies').to route_to('case_studies#create')
+      expect(:post => '/case_study_items').to route_to('case_study_items#create')
     end
 
     it 'routes to #update via PUT' do
-      expect(:put => '/case_studies/1').to route_to('case_studies#update', :id => '1')
+      expect(:put => '/case_study_items/1').to route_to('case_study_items#update', :id => '1')
     end
 
     it 'routes to #update via PATCH' do
-      expect(:patch => '/case_studies/1').to route_to('case_studies#update', :id => '1')
+      expect(:patch => '/case_study_items/1').to route_to('case_study_items#update', :id => '1')
     end
 
     it 'routes to #destroy' do
-      expect(:delete => '/case_studies/1').to route_to('case_studies#destroy', :id => '1')
+      expect(:delete => '/case_study_items/1').to route_to('case_study_items#destroy', :id => '1')
     end
 
   end
